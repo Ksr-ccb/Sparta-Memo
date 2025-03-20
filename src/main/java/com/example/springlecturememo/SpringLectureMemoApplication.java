085674e5b301144853378e5f2aaacce70feb8518
ksr-ccb <harang4282@gmail.com>
2025-03-20 17:24:43 +0900

    🎉 Initial setup of the project

---
+++ src/main/java/com/example/springlecturememo/SpringLectureMemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlecturememo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLectureMemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLectureMemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springlecturememo/SpringLectureMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlecturememo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLectureMemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
